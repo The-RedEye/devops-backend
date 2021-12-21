2be2d055042ecbaf672d85324bcccaf875af942f
The-RedEye <setswipe@gmail.com>
2021-12-21 12:40:50 -0500

    Seed data completed and schema fixing db/seed.js functioning - deletes and re-seeds projects, seeds and users added parameters for schema for future implementation. user.admin, user.photo

---
+++ db/seeds.js
@@ -0,0 +1,25 @@
+const Project = require('../models/project')
+const ProjectSeed = require('./projectSeed.json')
+const Task = require('../models/task')
+const TaskSeed = require('./taskSeed.json')
+const User = require('../models/user')
+const UserSeed = require('./userSeed.json')
+
+Project.deleteMany({})
+  .then( () => Project.insertMany(ProjectSeed))
+  .then( console.log )
+  .catch( console.error )
+  .finally( () => { process.exit() })
+
+  Task.deleteMany({})
+  .then( () => Task.insertMany(TaskSeed))
+  .then( console.log )
+  .catch( console.error )
+  .finally( () => { process.exit() })
+
+  User.deleteMany({})
+  .then( () => User.insertMany(UserSeed))
+  .then( console.log )
+  .catch( console.error )
+  .finally( () => { process.exit() })
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 4000)
 
 app.use(express.json())
 app.use(express.urlencoded( {extended: true} ))
