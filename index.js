d82b74bfcd9bc11dfe4fe19a7875e685fd987e4d
The-RedEye <setswipe@gmail.com>
2021-12-21 18:00:20 -0500

    cors implementation completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.set('port', process.env.PORT || 4000)
 
 app.use(express.json())
 app.use(express.urlencoded( {extended: true} ))
+app.use(cors())
 
 //Controllers --------------------
 
